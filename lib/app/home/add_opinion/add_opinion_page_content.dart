016af1d427786c3e508d5aa61e4feaaa9d89e999
Grzegorz Cichioń <grzegorz@cichon.dev>
2022-05-27 19:12:06 +0200

    Implement adding filaments

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,16 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class AddFilamentPageContent extends StatelessWidget {
+class AddFilamentPageContent extends StatefulWidget {
   const AddFilamentPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddFilamentPageContent> createState() => _AddFilamentPageContentState();
+}
+
+class _AddFilamentPageContentState extends State<AddFilamentPageContent> {
+  var producentName = '';
+  var filamentName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Text(
-      'Dodaj swoją opinie o drukarce 3D',
-      style: GoogleFonts.courgette(),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę producenta',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                producentName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę filamentu',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                filamentName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('Filament').add({
+                'name': producentName,
+                'material': filamentName,
+                'rating': 4.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          )
+        ],
+      ),
     );
   }
 }
